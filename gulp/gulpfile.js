e3bb4dab04b627c00b68f074f02ffcbb7522156c
docmaster <prosto24061989@gmail.com>
2020-07-31 13:09:22 +0300

    gulp add

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,26 +1,22 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var autoprefix = require('gulp-autoprefixer');
+'use strict';
 
-// gulp.task('styles', function () {
-//     return gulp.src('less/**/*.less')
-//         .pipe(less())
-//         .pipe(autoprefix('last 10 version'))
-//         .pipe(gulp.dest('./'))
-// });
+const gulp = require('gulp');
+const less = require('gulp-less');
+const LessAutoprefix = require('less-plugin-autoprefix');
+const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+ 
 
-//Таск на стили CSS
+
 function styles() {
-   return gulp.src('less/**/*.less')
-   .pipe(autoprefix('last 10 version'))
-   .pipe(less())
-   .pipe(gulp.dest('./../src/static/css/'))
-}
-
-function build(){
-    gulp.watch('less/**/*.less', styles)
-}
-
-gulp.task('default', build)
-
-
+    return gulp.src('less/**/*.less')
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+    .pipe(gulp.dest('./../src/static/css/'));
+ }
+ 
+ function build(){
+     gulp.watch('less/**/*.less', styles)
+ }
+ 
+ gulp.task('default', build)
